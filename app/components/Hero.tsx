6145053ee2788ba3f10af5ca6e5a646f7b2860ec
lenaf <lforti41@gmail.com>
2023-08-25 21:30:17 -0400

    lg screen

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -66,7 +66,7 @@ export function Hero({
       </div>
       <div className="flex flex-col items-baseline justify-between gap-4 px-6 py-8 sm:px-8 md:px-12 bg-gradient-to-t dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
         {heading?.value && (
-          <h2 className="max-w-md font-bold text-4xl sm:text-5xl">
+          <h2 className="max-w-md font-bold text-4xl sm:text-5xl lg:text-6xl">
             {heading.value}
           </h2>
         )}
